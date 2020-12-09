eef6cd0abcd8a58cdbb30873bfb92933689cd120
Wilson <wilson@@freshturf.org>
2020-12-09 19:07:30 +0800

    -Updated serverless.yml to declare multi events.

--- src/app.ts
+++ src/app.ts
@@ -35,7 +35,7 @@ app.set("view engine", "pug");
 // parse application/json
 app.use(logger("dev"));
 app.use(helmet());
-// app.use(express.json());
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 //app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
